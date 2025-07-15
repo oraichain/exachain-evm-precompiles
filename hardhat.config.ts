db3bef6786cf58b45832849e3f1e09d7dae0762a
dang.vh <dang.vh2@orai.io>
2025-07-15 09:35:53 +0700

    scripts test obridge

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,13 +7,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
-const PRIVATE_KEY1 = process.env.PRIVATE_KEY2 || "0x0000000000000000000000000000000000000000000000000000000000000000";
-const PRIVATE_KEY2 = process.env.PRIVATE_KEY2 || "0x0000000000000000000000000000000000000000000000000000000000000000";
-const PRIVATE_KEY3 = process.env.PRIVATE_KEY3 || "0x0000000000000000000000000000000000000000000000000000000000000000";
-const PRIVATE_KEY4 = process.env.PRIVATE_KEY4 || "0x0000000000000000000000000000000000000000000000000000000000000000";
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -36,9 +29,9 @@ const config: HardhatUserConfig = {
       chainId: 20250626,
     },
     exachain: {
-      url: "http://128.199.120.187:8545",
-      chainId: 20250626,
-      accounts: [PRIVATE_KEY, PRIVATE_KEY1, PRIVATE_KEY2, PRIVATE_KEY3, PRIVATE_KEY4],
+      url: "https://evm.zken.io",
+      chainId: 20250711,
+      accounts: [PRIVATE_KEY],
       timeout: 120000, // 2 minutes timeout
       httpHeaders: {
         "User-Agent": "hardhat",
@@ -51,20 +44,20 @@ const config: HardhatUserConfig = {
       blockGasLimit: 30000000,
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-    coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "ETH",
-  },
-  etherscan: {
-    apiKey: {
-      mainnet: ETHERSCAN_API_KEY,
-      sepolia: ETHERSCAN_API_KEY,
-      polygon: process.env.POLYGONSCAN_API_KEY || "",
-      bsc: process.env.BSCSCAN_API_KEY || "",
-    },
-  },
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: "USD",
+  //   coinmarketcap: COINMARKETCAP_API_KEY,
+  //   token: "ETH",
+  // },
+  // etherscan: {
+  //   apiKey: {
+  //     mainnet: ETHERSCAN_API_KEY,
+  //     sepolia: ETHERSCAN_API_KEY,
+  //     polygon: process.env.POLYGONSCAN_API_KEY || "",
+  //     bsc: process.env.BSCSCAN_API_KEY || "",
+  //   },
+  // },
   paths: {
     sources: "./contracts",
     tests: "./test",
--- scripts/interaction/IBCTransfer.ts
+++ scripts/interaction/IBCTransfer.ts
@@ -1,8 +1,38 @@
 import { ethers } from "hardhat";
 
+// async function main() {
+//   const amt = ethers.parseEther("0.000000000000001000"); // 3000
+//   const receiver = "noble143h7yhvp595g905u49w8r34x6fc0x9tsvczknj";
+//   const precompile = await ethers.getContractAt(
+//     "ICS20I",
+//     "0x0000000000000000000000000000000000000802"
+//   );
+
+//   const signers = await ethers.getSigners();
+//   console.log("signers", signers[0].address);
+//   const tx = await precompile.transfer(
+//     "transfer",
+//     "channel-3",
+//     "uusdx",
+//     amt,
+//     signers[0].address,
+//     receiver,
+//     { revisionNumber: 1, revisionHeight: 10000000000 },
+//     0,
+//     ""
+//   );
+//   const receipt = await tx.wait(1);
+
+//   console.log(
+//     `Transferred ${ethers.formatEther(amt)} USDX via IBC to ${receiver}`
+//   );
+//   console.log("The transaction details are");
+//   console.log(receipt);
+// }
+
 async function main() {
-  const amt = ethers.parseEther("0.000000000000003000"); // 3000
-  const receiver = "noble143h7yhvp595g905u49w8r34x6fc0x9tsvczknj";
+  const amt = ethers.parseEther("0.000000000000020000"); // 3000
+  const receiver = "oraib1qpuundpvtymcyq3cmcty3udf2zy0m509e5jykd";
   const precompile = await ethers.getContractAt(
     "ICS20I",
     "0x0000000000000000000000000000000000000802"
@@ -12,19 +42,19 @@ async function main() {
   console.log("signers", signers[0].address);
   const tx = await precompile.transfer(
     "transfer",
-    "channel-0",
+    "channel-4",
     "uusdx",
     amt,
     signers[0].address,
     receiver,
-    { revisionNumber: 1, revisionHeight: 10000000000 },
+    { revisionNumber: 2, revisionHeight: 10000000000 },
     0,
-    ""
+    "oraib0x7eaf74eA145a5A81764EC2ce5fb40cd06DFDD18f"
   );
   const receipt = await tx.wait(1);
 
   console.log(
-    `Transferred ${ethers.formatEther(amt)} EVMOS via IBC to ${receiver}`
+    `Transferred ${ethers.formatEther(amt)} USDX via IBC to ${receiver}`
   );
   console.log("The transaction details are");
   console.log(receipt);
